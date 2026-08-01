2c8d6edbe88d32fd96fd00c4553139e5a8dca6a3
Justin0828 <panyz24@mails.tsinghua.edu.cn>
2026-08-01 20:37:35 +0800

    docs: add final defense presentation

--- thubeamer/makebeamer-en.bat
+++
@@ -1,5 +0,0 @@
-latexmk -c main-en
-pdflatex -synctex=1 -interaction=nonstopmode main-en
-bibtex main-en
-pdflatex -synctex=1 -interaction=nonstopmode main-en
-pdflatex -synctex=1 -interaction=nonstopmode main-en
--- thubeamer/makedoc.bat
+++
@@ -1,5 +0,0 @@
-xelatex thubeamer.dtx
-makeindex -s gind.ist -o thubeamer.ind thubeamer.idx
-makeindex -s gglo.ist -o thubeamer.gls thubeamer.glo
-xelatex thubeamer.dtx
-xelatex thubeamer.dtx
